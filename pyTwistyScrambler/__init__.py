a03e3d2e8921036b18ebe8a90130f3c8c1c730ff
Wes Evans <euphwes@gmail.com>
2017-02-16 21:40:03 -0700

    Added following additional scramble types for 3x3x3: edges, corners, LL, LSLL, ZBLL, ZZLL, F2L, LSE, CMLL, CLL, ELL.

--- pyTwistyScrambler/__init__.py
+++ pyTwistyScrambler/__init__.py
@@ -3,17 +3,17 @@ from os import path
 curr_dir = path.dirname(path.realpath(__file__))
 
 def trim(func):
-	def trimmed_func():
-		return func().strip()
-	return trimmed_func
+    def trimmed_func():
+        return func().strip()
+    return trimmed_func
 
 #------------------------------------------------------------------------------
 
 with open(path.join(curr_dir, 'js_resources/mathlib.js')) as f:
-	MATHLIB_SRC = f.read()
+    MATHLIB_SRC = f.read()
 
 with open(path.join(curr_dir, 'js_resources/cross.js')) as f:
-	CROSS_SRC = f.read()
+    CROSS_SRC = f.read()
 
 with open(path.join(curr_dir, 'js_resources/scramble_333_edit.js')) as f:
-	SCRAMBLE_333_SRC = f.read()
+    SCRAMBLE_333_SRC = f.read()
--- pyTwistyScrambler/scrambler333.py
+++ pyTwistyScrambler/scrambler333.py
@@ -7,4 +7,60 @@ _scrambler = execjs.compile(MATHLIB_SRC + CROSS_SRC + SCRAMBLE_333_SRC)
 
 @trim
 def get_random_scramble():
-	return _scrambler.call("scramble_333.getRandomScramble")
+    """ Gets a random scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getRandomScramble")
+
+@trim
+def get_edges_scramble():
+    """ Gets an edges-only scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getEdgeScramble")
+
+@trim
+def get_corners_scramble():
+    """ Gets a corners-only scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getCornerScramble")
+
+@trim
+def get_LL_scramble():
+    """ Gets a last layer scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getLLScramble")
+
+@trim
+def get_LSLL_scramble():
+    """ Gets a last slot last layer scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getLSLLScramble")
+
+@trim
+def get_ZBLL_scramble():
+    """ Gets a Zborowski-Bruchem last layer scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getZBLLScramble")
+
+@trim
+def get_ZZLL_scramble():
+    """ Gets a Zbigniew Zborowski last layer scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getZZLLScramble")
+
+@trim
+def get_F2L_scramble():
+    """ Gets an F2L (first two layers) scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getF2LScramble")
+
+@trim
+def get_LSE_scramble():
+    """ Gets an LSE (last six edges) scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getLSEScramble")
+
+@trim
+def get_CMLL_scramble():
+    """ Gets a CMLL scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getCMLLScramble")
+
+@trim
+def get_CLL_scramble():
+    """ Gets a CLL scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getCLLScramble")
+
+@trim
+def get_ELL_scramble():
+    """ Gets an ELL scramble of a 3x3x3 cube. """
+    return _scrambler.call("scramble_333.getELLScramble")
