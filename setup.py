bb1fb2a03317b68680c799dd78e79601d92b2ee1
Wes Evans <euphwes@gmail.com>
2018-05-31 21:36:23 -0600

    Added dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="pyTwistyScrambler",
-    version="1.0",
+    version="1.1",
     author="euphwes",
     author_email="euphwes@gmail.com",
     url="https://github.com/euphwes/pyTwistyScrambler",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'appdirs',
+        'packaging',
+        'PyExecJS',
+        'pyparsing',
+        'six',
+    ],
 )
