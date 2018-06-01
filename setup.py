9402e7e4ebc08864954e09c8423a662bc423cf67
Wes Evans <euphwes@gmail.com>
2018-05-31 21:57:34 -0600

    Added manifest to help include JS resources, bumped version.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import setuptools
 
 setuptools.setup(
     name="pyTwistyScrambler",
-    version="1.1",
+    version="1.2",
     author="euphwes",
     author_email="euphwes@gmail.com",
     url="https://github.com/euphwes/pyTwistyScrambler",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'appdirs',
         'packaging',
