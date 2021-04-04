cd07b7072c122fe00e7d4bfa60cfc9bd1d5a70b6
Wes Evans <euphwes@gmail.com>
2021-04-04 10:04:36 -0600

    Bump to 1.6 release for PyPi, for FTO scrambler upgrade and Rex Cube addition.

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="pyTwistyScrambler",
-    version="1.3",
+    version="1.6",
     author="euphwes",
     author_email="euphwes@gmail.com",
-    url="https://github.com/euphwes/pyTwistyScrambler",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    description="A Python utility package for generating scrambles for twisty puzzles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/euphwes/pyTwistyScrambler",
+    project_urls={
+        "Bug Tracker": "https://github.com/euphwes/pyTwistyScrambler/issues",
+    },
     install_requires=[
         'appdirs',
         'packaging',
